094714b60611611d61393e2ad93f837b4dd1d272
OLGA <popovaolgaig@gmail.com>
2026-05-21 10:42:23 +0200

    Sync: 2026-05-21 10:42:23

---
+++ app/public/wp-content/uploads/uag-plugin/assets/0/uag-js-13.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-4a635190' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-f5b427b7' );
+});
+ });
