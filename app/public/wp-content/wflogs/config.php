c485bf76eedb845a1f103aa4886e43dacd8e6c07
OLGA <popovaolgaig@gmail.com>
2026-05-21 10:52:47 +0200

    chore: remove wflogs from tracking

--- app/public/wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1779874380;s:7:"authKey";s:64:"#wgc+]YSg9a~]AV ~W<PgBXoP,e;:p,/J]v[UX8^Opc;jNO/UNwGpMABrT4oAQqr";s:7:"version";s:5:"1.1.0";}
